5b35158f268f7fde21546b71e249ffee7dd5d7c0
unknown <alextq3036@gmail.com>
2021-11-02 16:57:19 -0400

    Created the navbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from './components/navbar.component';
+
 function App() {
   return (
     <Router>
-      <div className="App">
-        hello
-      </div>
+      <Navbar />
+      <br />
+      {/*
+      <Route exact path='/' component={ExerciseList} />
+      <Route path='/edit/:id' component={EditExercise} />
+      <Route path='/create' component={CreateExercise} />
+      <Route path='/user' component={CreateUser} />
+      */}
     </Router>
   );
 }
