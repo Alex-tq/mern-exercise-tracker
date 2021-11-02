fb48be6f3b54c11d76f76f56df9663ba4477cbb7
unknown <alextq3036@gmail.com>
2021-11-02 03:25:35 -0400

    created the users and exercises routes

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,13 @@ connection.once('open', ()=>{
     console.log("MongoDB database connection established successfully")
 })
 
+const exercisesRouter = require('./routes/exercises')
+const usersRouter = require('./routes/users')
+
+app.use('/exercises', exercisesRouter)
+app.use('/users', usersRouter)
+
+
 //Starts the server. starts listening on a specific port.
 app.listen(port, ()=>{
     console.log(`server is runnig on port: ${port}`)
